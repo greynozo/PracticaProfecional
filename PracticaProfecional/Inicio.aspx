b47eecfae96dbafadb819d218cf0fa611aa7a0a6
greynozo <kioc04@hotmail.com>
2023-05-14 19:14:04 -0300

    Initial commit

---
+++ PracticaProfecional/Inicio.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="PracticaProfecional.Inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="content" class="p-4 p-md-5 pt-5">
+        <h2 class="mb-4">Sidebar #05</h2>
+        <form runat="server">
+        <table>
+            <tr>
+                <td><asp:Label runat="server" Text="Nombre"></asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtNombre"></asp:TextBox></td>
+                <td> <asp:RequiredFieldValidator ID="RequiredFieldValidator1" ControlToValidate="txtNombre" runat="server" Text="*" ErrorMessage="El campo no puede estar vacio" ForeColor="Red"></asp:RequiredFieldValidator></td>
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" Text="Apellido"></asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtApellido"></asp:TextBox></td>
+                <td> <asp:RequiredFieldValidator ErrorMessage="El apellido es obligatorio" Text="*" ForeColor="Red" ControlToValidate="txtApellido" runat="server" /> </td>
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" Text="Edad"></asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtEdad"></asp:TextBox></td>
+                <td><asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="txtEdad" ErrorMessage="La edad debe ser mayor a 16" Text="*" MinimumValue="16" MaximumValue="99"></asp:RangeValidator></td>
+                
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" Text="Password"></asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtPassword" TextMode="Password"></asp:TextBox></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" Text="Repetir Password"></asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtRepPassword"></asp:TextBox></td>
+                <td><asp:CompareValidator runat="server" ID="cmpValidator" Text="*" ErrorMessage="Los campos deben ser iguales." ControlToValidate="txtPassword" ControlToCompare="txtRepPassword" ForeColor="Red" ></asp:CompareValidator> </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button runat="server" CssClass="btn-success" Text="Enviar" ID="btnEnviar"/>
+                    <asp:ValidationSummary ID="ValidationSummary1" ForeColor="Green" runat="server" />
+                </td>
+            </tr>
+        </table>    
+            </form>
+    </div>
+</asp:Content>
---
+++ PracticaProfecional/Inicio.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PracticaProfecional
+{
+
+
+    public partial class Inicio
+    {
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// Control txtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellido;
+
+        /// <summary>
+        /// Control txtEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEdad;
+
+        /// <summary>
+        /// Control RangeValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
+
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        /// <summary>
+        /// Control txtRepPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRepPassword;
+
+        /// <summary>
+        /// Control cmpValidator.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator cmpValidator;
+
+        /// <summary>
+        /// Control btnEnviar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEnviar;
+
+        /// <summary>
+        /// Control ValidationSummary1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+    }
+}
